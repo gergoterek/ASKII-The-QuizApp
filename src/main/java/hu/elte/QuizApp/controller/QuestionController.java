89c6d823a5707927739ac8a228398d68c874d338
Kalman Fanni <kalman12fanni@gmail.com>
2020-03-06 12:27:29 +0100

    Create post, get, delete in QuestionController

--- src/main/java/hu/elte/QuizApp/controller/QuestionController.java
+++ src/main/java/hu/elte/QuizApp/controller/QuestionController.java
@@ -1,13 +1,12 @@
 package hu.elte.QuizApp.controller;
 
-import hu.elte.QuizApp.model.Label;
 import hu.elte.QuizApp.model.Question;
 import hu.elte.QuizApp.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
@@ -15,18 +14,31 @@ public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
 
-/*
-    @Autowired
-    private LabelRepository labelRepository;
-*/
 
+    //get - http://localhost:8080/question
     @GetMapping("")
     public Iterable<Question> getQuestions(){
         return questionRepository.findAll();
     }
 
-    /*@GetMapping("")
-    public Iterable<Label> getLabels(){
-        return labelRepository.findAll();
-    }*/
+    @PostMapping("")
+    public ResponseEntity<Question> createQuestion(@RequestBody Question question)
+    {
+        Question savedQuestion = questionRepository.save(question);
+        return ResponseEntity.ok(savedQuestion);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteQuestion(
+            @PathVariable Integer id
+    ) {
+        try {
+            questionRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
