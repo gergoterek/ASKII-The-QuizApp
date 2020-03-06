bf07e4c001b4a0989c7d10e6bde3a9b77de6fba1
Menetke <43723558+Menetke@users.noreply.github.com>
2020-03-06 22:45:37 +0100

    Added add_question component

--- frontend-src/src/app/app.module.ts
+++ frontend-src/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { QuestionComponent } from './question/question.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    QuestionComponent
+    QuestionComponent,
+    AddQuestionComponent
   ],
   imports: [
     BrowserModule,
